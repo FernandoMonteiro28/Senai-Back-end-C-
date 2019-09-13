e8b016b9e65878c5bd8c4750ecf05fb19b238b65
FIC <FIC@info.local>
2019-09-13 15:13:16 -0300

    alteração de nome

---
+++ Exercicios de C#/Exercicio 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float salariofx, totalvd, sfinal;
+
+            Console.WriteLine("Digite seu salario");
+            salariofx = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o total da suas vendas");
+            totalvd = float.Parse(Console.ReadLine());
+
+            sfinal = salariofx + (totalvd *1.05f);
+
+            Console.WriteLine($"O valor final é: {sfinal}");
+
+
+        }
+    }
+}
